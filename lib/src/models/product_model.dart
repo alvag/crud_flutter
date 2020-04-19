dc9150450a45698ab1602083ac7e794f05039580
Max Alva <malva@peruapps.com.pe>
2020-04-18 22:04:33 -0500

    ProductModel

---
+++ lib/src/models/product_model.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+ProductModel productModelFromJson(String str) =>
+    ProductModel.fromJson(json.decode(str));
+
+String productModelToJson(ProductModel data) => json.encode(data.toJson());
+
+class ProductModel {
+  String id;
+  String title;
+  double price;
+  bool available;
+  String photo;
+
+  ProductModel({
+    this.id,
+    this.title = '',
+    this.price = 0.0,
+    this.available = true,
+    this.photo,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"],
+        available: json["available"],
+        photo: json["photo"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "available": available,
+        "photo": photo,
+      };
+}
