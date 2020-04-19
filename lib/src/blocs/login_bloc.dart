68e0a445166a150e6506d1b179d3fb92e1a71b7e
Max Alva <malva@peruapps.com.pe>
2020-04-18 19:40:04 -0500

    StreamTransformer y Validaciones

--- lib/src/blocs/login_bloc.dart
+++ lib/src/blocs/login_bloc.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
-class LoginBloc {
+import 'package:crud_flutter/src/blocs/validators.dart';
+
+class LoginBloc with Validators {
   final _emailCtrl = StreamController<String>.broadcast();
   final _passwordCtrl = StreamController<String>.broadcast();
 
   // recuperar los datos del stream
-  Stream<String> get emailStream => _emailCtrl.stream;
-  Stream<String> get passwordStream => _passwordCtrl.stream;
+  Stream<String> get emailStream => _emailCtrl.stream.transform(emailValidator);
+  Stream<String> get passwordStream =>
+      _passwordCtrl.stream.transform(passwordValidator);
 
   // insertar valores al stream
   Function(String) get changeEmail => _emailCtrl.sink.add;
