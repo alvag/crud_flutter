0de4f6490413ea7135914401bd1d41d7fc2830b0
Max Alva <malva@peruapps.com.pe>
2020-04-18 20:42:23 -0500

    Provider singleton

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -3,9 +3,21 @@ import 'package:crud_flutter/src/blocs/login_bloc.dart';
 export 'package:crud_flutter/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget {
+  static Provider _instance;
+
+  factory Provider({Key key, Widget child}) {
+    if (_instance == null) {
+      _instance = new Provider._(key: key, child: child);
+    }
+
+    return _instance;
+  }
+
+  Provider._({Key key, Widget child}) : super(key: key, child: child);
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  // Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
